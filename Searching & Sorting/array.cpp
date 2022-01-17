ed0a8de4f8fb2103c0a02bc1df2fc0fa6f31cb35
yugal82 <91265372+yugal82@users.noreply.github.com>
2022-01-18 00:30:11 +0530

    Arrays

---
+++ Searching & Sorting/array.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<conio.h>
+#define MAX 10
+using namespace std;
+
+int main()
+{
+    int i,n;
+    int x[MAX];
+    cout<<"\n Enter the range: ";
+    cin>>n;
+
+    for(i=0; i<n; i++)
+    {
+        cout<<"Enter element to be inserted: ";
+        cin>>x[i];
+    }
+
+    cout<<"\nThe elements in the array are:";
+    for(i=0; i<n; i++)
+    {
+        cout<<" "<<x[i];
+    }
+    
+    return 0;
+}
